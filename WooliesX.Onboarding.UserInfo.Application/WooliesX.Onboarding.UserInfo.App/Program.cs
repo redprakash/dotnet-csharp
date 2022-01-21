b10ef2ef65c8db9fc0e98da689525025ed63ebf0
Prakash <bhaktpoudel@gmail.com>
2022-01-21 13:09:49 +1100

    Refactored Project UserInfo

---
+++ WooliesX.Onboarding.UserInfo.Application/WooliesX.Onboarding.UserInfo.App/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace WooliesX.Onboarding.UserInfo.App
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("\n Please enter your username");
+            var userInputUserName = Console.ReadLine();
+            Console.WriteLine("\n Please enter your password");
+            var userInputPassword = Console.ReadLine();
+
+            UserInfoProcessor processor = new UserInfoProcessor();
+            if(processor.ValidateUser(userInputUserName, userInputPassword))
+            {
+                Console.WriteLine("Validated");
+            }
+            else
+            {
+                Console.WriteLine("Invalid");
+            }
+        }
+    }
+}
---
+++ WooliesX.Onboarding.UserInfo.Application/WooliesX.Onboarding.UserInfo.App/UserInfoProcessor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WooliesX.Onboarding.UserInfo.App
+{
+    internal class UserInfoProcessor
+    {
+        private readonly string loginUserName = "admin";
+        private readonly string loginUserPassword = "admin";
+
+        
+        public bool ValidateUser(string userName, string userPassword)
+        {
+            if(userName == loginUserName && userPassword == loginUserPassword)
+            {
+                return true;
+            }
+                return false;
+          
+        }
+
+    }
+}
