b42a9c841680375aab1ceb71fd8352f40c9d1d92
Prakash <bhaktpoudel@gmail.com>
2022-01-24 13:13:37 +1100

    Creating MvcMovie Web App
    Learning MVC with MSDN

---
+++ MvcMovie/Controllers/BlogController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MvcMovie.Controllers
+{
+    public class BlogController : Controller
+    {
+        public IActionResult MyBlog()
+        {
+            return View();
+        }
+    }
+}
---
+++ MvcMovie/Models/MovieGenreViewModel.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+namespace MvcMovie.Models
+{
+    public class MovieGenreViewModel
+    {
+
+    }
+}
